8439e44a6992289091638a6b38b9253ba4ff7a84
amanjaiswa1 <amanjaiswal0608@gmail.com>
2022-09-06 01:41:28 +0530

    Add: Calculate Length Of A Line

---
+++ LineComparisonComputation.java
@@ -0,0 +1,24 @@
+package com.assignments.day3.LineComparision;
+
+import java.util.*;
+
+public class LineComparisonComputation {
+  public static void main(String[] args) {
+	  System.out.println("Welcome to Line Comparison Computation Program");
+
+      Scanner sc = new Scanner(System.in);
+
+      System.out.println("Enter the value of line XY:");
+      System.out.println("x1 coordinate:");
+      int x1 = sc.nextInt();
+      System.out.println("y1 coordinate:");
+      int y1 = sc.nextInt();
+      System.out.println("x2 coordinate:");
+      int x2 = sc.nextInt();
+      System.out.println("y2 coordinate:");
+      int y2 = sc.nextInt();
+
+      double lineLengthXY = Math.sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+      System.out.println("\nLength of the line XY is: "+lineLengthXY);
+  }
+  }
